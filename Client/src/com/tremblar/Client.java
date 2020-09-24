65b71d8c7a0cd92d191edf46d40a0667ddc6c05e
William Décoste <widec@me.com>
2020-09-24 18:46:37 -0400

    Refactoring entry validations in separete class

--- Client/src/com/tremblar/Client.java
+++ Client/src/com/tremblar/Client.java
@@ -12,42 +12,16 @@ public class Client {
 		scan = new Scanner(System.in);
 		
 		String serverAddress;
-		boolean isValidServerAddress = false;
 		do {
 			System.out.print("Enter the server IP: ");
 			serverAddress = scan.next();
-			String[] addressBytes = serverAddress.split("\\.");
-			if (addressBytes.length != 4) {
-				System.out.println("Server IP must have 4 bytes");
-				continue;
-			}
-			
-			isValidServerAddress = true;
-			for (int i = 0; i < addressBytes.length; i++) {
-				try {
-					isValidServerAddress &= Integer.parseInt(addressBytes[i]) >= 0;
-					isValidServerAddress &= Integer.parseInt(addressBytes[i]) <= 255;
-				} catch (NumberFormatException e) {
-					System.out.println("Not all entries were numbers - " + e);
-					isValidServerAddress = false;
-				}
-				if (!isValidServerAddress) {
-					System.out.println("Server IP can only contain numbers between 0 and 255");
-					break;
-				}
-			}
-			
-		} while (!isValidServerAddress);
+		} while (!EntryValidator.verifyIpAddress(serverAddress));
 		
 		int serverPort;
 		do {
 			System.out.print("Enter the server port: ");
 			serverPort = scan.nextInt();
-			
-			if (serverPort < 5000 || serverPort > 5050) {
-				System.out.println("Server port must be between 5000 and 5050");
-			}
-		} while (serverPort < 5000 || serverPort > 5050);
+		} while (!EntryValidator.verifyPort(serverPort));
 		
 		connection = new ServerConnection(serverAddress, serverPort);
 		
